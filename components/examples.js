d25ea7d8d9f605ae6a38fd0c5d0780602f1c536c
Tristan Guest <tristanguest@gmail.com>
2022-11-13 09:36:09 -0400

    Omit example text

--- components/examples.js
+++ components/examples.js
@@ -1,93 +1,93 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function App() {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+// export default function App() {
+//   const [data, setData] = useState();
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      const response = await fetch("/api/route");
-      const data = await response.json();
-      setData(data);
-      setLoading(false);
-    };
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       setLoading(true);
+//       const response = await fetch("/api/route");
+//       const data = await response.json();
+//       setData(data);
+//       setLoading(false);
+//     };
 
-    fetchData();
-  }, []);
+//     fetchData();
+//   }, []);
 
-  return <>{data && data}</>;
-}
+//   return <>{data && data}</>;
+// }
 
-//
+// //
 
-// import { useState, useEffect, useRef } from "react";
+// // import { useState, useEffect, useRef } from "react";
 
-function useFetch(url) {
-  const cache = useRef({});
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+// function useFetch(url) {
+//   const cache = useRef({});
+//   const [data, setData] = useState();
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      if (cache.current[url]) {
-        setData(cache.current[url]);
-        setLoading(false);
-      } else {
-        const response = await fetch(url);
-        const data = await response.json();
-        cache.current[url] = data;
-        setData(data);
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [url]);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       setLoading(true);
+//       if (cache.current[url]) {
+//         setData(cache.current[url]);
+//         setLoading(false);
+//       } else {
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         cache.current[url] = data;
+//         setData(data);
+//         setLoading(false);
+//       }
+//     };
+//     fetchData();
+//   }, [url]);
 
-  return { data, loading, error };
-}
+//   return { data, loading, error };
+// }
 
-export function Appp() {
-  const { data: userData } = useFetch("/api/user");
-  const { data: userPosts } = useFetch("/api/user/posts");
-  return <>{"..."}</>;
-}
+// export function Appp() {
+//   const { data: userData } = useFetch("/api/user");
+//   const { data: userPosts } = useFetch("/api/user/posts");
+//   return <>{"..."}</>;
+// }
 
-//
+// //
 
-import useSWR from "swr";
+// import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
+// const fetcher = (url) => fetch(url).then((res) => res.json());
 
-function Apppp() {
-  const { data, error } = useSWR("/api/route", fetcher);
-  return (
-    <div className="App">
-      {!error && <p>{data}</p>}
-      ...
-    </div>
-  );
-}
+// function Apppp() {
+//   const { data, error } = useSWR("/api/route", fetcher);
+//   return (
+//     <div className="App">
+//       {!error && <p>{data}</p>}
+//       ...
+//     </div>
+//   );
+// }
 
-//
+// //
 
-const useData = (url) => {
-  const { data, error } = useSWR(url, fetcher);
-  return { data, error };
-};
+// const useData = (url) => {
+//   const { data, error } = useSWR(url, fetcher);
+//   return { data, error };
+// };
 
-function Appppp() {
-  const { data: userData, ...status1 } = useData("/api/user");
-  const { data: userPosts, ...status2 } = useData("/api/posts");
-  return (
-    <div className="App">
-      {!error && <p>{data}</p>}
-      ...
-      <Card />
-      <Card />
-    </div>
-  );
-}
+// function Appppp() {
+//   const { data: userData, ...status1 } = useData("/api/user");
+//   const { data: userPosts, ...status2 } = useData("/api/posts");
+//   return (
+//     <div className="App">
+//       {!error && <p>{data}</p>}
+//       ...
+//       <Card />
+//       <Card />
+//     </div>
+//   );
+// }
